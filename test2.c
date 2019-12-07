9b19d6eaf0dc00ceba1e73b59d0f712a44625ddf
k2j507 <rloveyou777@naver.com>
2019-12-07 19:58:09 +0900

    test2.c

---
+++ test2.c
@@ -0,0 +1,29 @@
+int max = 5000;
+
+void main(){
+	int c;
+	int arr[2] = {1,2};
+	c= arr[0];
+
+   int a=1;
+   int b=99;
+   print(add(a,b));
+   print(arrr[0]);
+
+}
+
+int add(int x, int y){
+   int z;
+   z = x +y;
+   return z;
+}
+
+int sum(int n){
+   int i =1;
+   int sum = 0;
+   while(i<n){
+      sum = sum + i;
+      ++i;
+   }
+   return sum;
+}
